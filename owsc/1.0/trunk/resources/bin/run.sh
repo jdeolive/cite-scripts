6405f5e4ed4fe72bb75d6c1a8fa9877982ebb40e
pgoncalves <pgoncalves@32909982-3df7-0310-a142-94324ff2e7a9>
2012-12-18 21:10:30 +0000

    [owc] defined a new rnc and rng for owc1.0
    git-svn-id: https://svn.opengeospatial.org:8443/ogc-projects/cite/scripts@5100 32909982-3df7-0310-a142-94324ff2e7a9

--- owsc/1.0/trunk/resources/bin/run.sh
+++ owsc/1.0/trunk/resources/bin/run.sh
@@ -3,18 +3,19 @@ PID=$$
 file=$1
 rule=$2
 conformance="../relaxng/"
-
+echo "java -jar ../lib/trang/trang.jar -I rnc -O rng $conformance/$rule.rnc $conformance/$rule.rng"
 java -jar ../lib/trang/trang.jar -I rnc -O rng $conformance/$rule.rnc $conformance/$rule.rng
 if [ "$?"  != "0" ] 
 then 
 	echo "FAILED to validate RelaxNG"
 	exit 2
 fi 
-
+echo "java -jar ../lib/relames/relames.jar $conformance/$rule.rng $file"
 res=`java -jar ../lib/relames/relames.jar $conformance/$rule.rng $file`
-if [ ! -z "`echo "$res" | grep "NOT valid"`" ] 
+#if [ ! -z "`echo "$res" | grep "NOT valid"`" ] 
+if [ "`printf %s "$res" | tail -1 `" != "valid" ] 
 then 
-	echo "`echo "$res" | grep -A 1 "Error at"`"
+	echo "`echo "$res" `" #| grep -A 1 "Error at"`"
 	echo "FAILED"
 	exit 1
 fi
