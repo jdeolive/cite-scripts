f530e6a19bbd2e1b9002d3dd16ac610eb15b9e35
pgoncalves <pgoncalves@32909982-3df7-0310-a142-94324ff2e7a9>
2012-12-18 21:23:30 +0000

    [owc] defined a new rnc and rng for owc1.0
    git-svn-id: https://svn.opengeospatial.org:8443/ogc-projects/cite/scripts@5104 32909982-3df7-0310-a142-94324ff2e7a9

--- owsc/1.0/trunk/resources/bin/A01.sh
+++ owsc/1.0/trunk/resources/bin/A01.sh
@@ -6,7 +6,7 @@ file="../../web/examples/meris_borders_users.atom"
 echo "OGC Context Conformance Test Classes (OGC 12-084)" >&2
 echo "A.1 Conformance Test Class: core " >&2
 echo "To test requirement http://www.opengis.net/spec/owc/1.0/conf/atom/atomRules" >&2
-sh run.sh $file "owc10_new2" >&2
+sh run.sh $file "owc10" >&2
 res=$?
 if [ $res  != "0" ] 
 then 
